d698873985c9863dd0c52e59f8c392209407094f
Documenter.jl <documenter@juliadocs.github.io>
2025-07-15 15:02:12 +0000

    build based on 7244882

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#FluxOptics","page":"Home","title":"FluxOptics","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FluxOptics.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
